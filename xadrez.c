893f4ab517e1cb522eeba11d7c9d8b28f574fc03
Groger-dev <gustavormsousa01@gmail.com>
2025-11-04 03:34:17 +0000

    Nível mestre codificado

--- xadrez.c
+++ xadrez.c
@@ -3,7 +3,7 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
+/*
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
@@ -72,6 +72,9 @@ int main() {
 
         printf("Esquerda\n"); //segundo movimento, uma casa para a esquerda;
     }
+    return 0;
+}
+    */
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
@@ -80,5 +83,77 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
+//Funções recursivas para cada peça;
+void movTorre (int n){
+        if (n > 0){
+            printf("Direita \n"); //Direção do movimento;
+            movTorre (n - 1); //Recursividade, decremento de n;
+        }
+    }
+    
+void movRainha (int n){
+    if (n > 0){
+        printf("Esquerda \n"); //Direção do movimento;
+        movRainha (n -1); //Recursividade, decremento de n;
+    }
 }
+void movBispo (int n){
+    if (n > 0){
+        printf("Cima direita \n"); //Direção do movimento;
+        movBispo (n - 1); //Recursividade, decremento de n;
+    }
+}
+
+int main (){
+
+    int casasT = 5, casasR = 8, casasB = 5, bispo1, bispo2, cavalo1, cavalo2, cavalo3; //variáveis para cada peça;
+
+    printf("Movimento da Torre:\n");
+    movTorre(casasT);
+
+    printf("\n"); //espaço para melhor vizualição na saída;
+
+    printf("Movimento da Rainha:\n");
+    movRainha(casasR);
+
+    printf("\n"); //espaço para melhor vizualição na saída;
+
+    //Por meio da recursividade
+    printf("Movimento do Bispo:\n");
+    
+    movBispo(casasB);
+
+    printf("Ou\n");
+
+    //Por meio do loop
+    for(bispo1 = 1; bispo1 <= 5; bispo1++){
+        for (bispo2 = 1; bispo2 < 2; bispo2++)
+        {
+            printf("Cima "); //Direção vertical com espaço para a outra direção;
+        }
+
+        printf("direita\n"); //Direção horizontal que somando com a anterior resultam na direção diagonal;
+        
+    }
+
+    printf("\n"); //espaço para melhor vizualição na saída;
+
+    printf("Movimento do Cavalo:\n");
+
+    
+    for (cavalo1 = 1, cavalo2 = 6; cavalo1 < cavalo2; cavalo1++, cavalo2--) //Loop avançado, duas variáveis que controlam o primeiro movimento;
+    {                
+        if (cavalo2 == 4) break; //Condição para encerrar o movimento da peça;
+        printf("Cima\n");
+
+        for(cavalo3 = 5; cavalo3 == cavalo2; cavalo3++) //Loop com condição para o segundo movimento;
+        {
+            printf("Direita\n");
+        }
+        
+        
+    }
+    
+
+    return 0;
+    }
